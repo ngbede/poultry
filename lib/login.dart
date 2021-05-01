c9d1be0b96f1b29fde24297a479abc8743cdc44d
Emmanuel <ogasule001@gmail.com>
2021-05-01 14:41:12 +0100

    add progress spinner to login and signup screen

--- lib/login.dart
+++ lib/login.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:poultry/config/fieldtype.dart';
+import 'package:poultry/config/enumvals.dart';
 import 'package:poultry/provider/user_prov.dart';
 import 'package:poultry/signup.dart';
 import 'package:poultry/widgets/inputfield.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
+
+import 'config/firebase.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -14,123 +17,134 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   GlobalKey<FormState> _signinFormkey = GlobalKey<FormState>();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Form(
-          key: _signinFormkey,
-          autovalidateMode: AutovalidateMode.disabled,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  bottom: 20.0,
-                  top: 20.0,
-                ),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Login to your account",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
+        body: ProgressHUD(
+          barrierEnabled: true,
+          child: Builder(
+            builder: (context) => Form(
+              key: _signinFormkey,
+              autovalidateMode: AutovalidateMode.disabled,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20.0,
+                      bottom: 20.0,
+                      top: 20.0,
                     ),
-                  ),
-                ),
-              ),
-              InputField(
-                name: "Email",
-                keyboard: TextInputType.emailAddress,
-                fieldType: FieldType.email,
-              ),
-              InputField(
-                name: "Password",
-                keyboard: TextInputType.visiblePassword,
-                iconVisible: true,
-                fieldType: FieldType.password,
-              ),
-              GestureDetector(
-                onTap: () async {
-                  if (_signinFormkey.currentState.validate()) {
-                    try {
-                      final UserCredential _user =
-                          await _auth.signInWithEmailAndPassword(
-                        email:
-                            Provider.of<UserProv>(context, listen: false).email,
-                        password: Provider.of<UserProv>(context, listen: false)
-                            .password,
-                      );
-                      if (_user != null) {
-                        // TODO: navigate to app menu
-                        print("Sign in successful");
-                      }
-                    } catch (e) {
-                      Fluttertoast.showToast(
-                        msg: "${e.code}",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.BOTTOM,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.teal,
-                        textColor: Colors.white,
-                        fontSize: 16.0,
-                      );
-                    }
-                  }
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Card(
-                    color: Colors.green[400],
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
                       child: Text(
-                        "Login",
-                        style: TextStyle(color: Colors.white),
-                        textAlign: TextAlign.center,
+                        "Login to your account",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Are you a new user?",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  InputField(
+                    name: "Email",
+                    keyboard: TextInputType.emailAddress,
+                    fieldType: FieldType.email,
+                  ),
+                  InputField(
+                    name: "Password",
+                    keyboard: TextInputType.visiblePassword,
+                    iconVisible: true,
+                    fieldType: FieldType.password,
                   ),
                   GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Signup(),
-                        ),
-                      );
-                      print("Navigate to Sign up Screen");
+                    onTap: () async {
+                      if (_signinFormkey.currentState.validate()) {
+                        final _progress = ProgressHUD.of(context);
+                        _progress.showWithText("Logging in...");
+                        await Future.delayed(
+                            Duration(seconds: 5)); // simulate progress
+                        try {
+                          final UserCredential _user =
+                              await auth.signInWithEmailAndPassword(
+                            email: Provider.of<UserProv>(context, listen: false)
+                                .email,
+                            password:
+                                Provider.of<UserProv>(context, listen: false)
+                                    .password,
+                          );
+                          if (_user != null) {
+                            // TODO: navigate to app menu
+                            _progress.dismiss();
+                            print("Sign in successful");
+                          }
+                        } on FirebaseAuthException catch (e) {
+                          _progress.dismiss();
+                          Fluttertoast.showToast(
+                            msg: "${e.code}",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.teal,
+                            textColor: Colors.white,
+                            fontSize: 16.0,
+                          );
+                        }
+                      }
                     },
-                    child: Text(
-                      " Sign up",
-                      style: TextStyle(
-                          color: Colors.teal,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Card(
+                        color: Colors.green[400],
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Are you a new user?",
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Signup(),
+                            ),
+                          );
+                          print("Navigate to Sign up Screen");
+                        },
+                        child: Text(
+                          " Sign up",
+                          style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/signup.dart
+++ lib/signup.dart
@@ -9,6 +9,7 @@ import 'package:poultry/widgets/inputfield.dart';
 import 'package:poultry/widgets/user_role_button.dart';
 import 'package:provider/provider.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 
 class Signup extends StatefulWidget {
   @override
@@ -26,178 +27,190 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Form(
-          key: _signupFormKey,
-          autovalidateMode: AutovalidateMode.disabled,
-          child: ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20, bottom: 20, top: 20),
-                child: Text(
-                  "Create your account",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),
-                ),
-              ),
-              InputField(
-                name: "First name",
-                keyboard: TextInputType.text,
-                fieldType: FieldType.firstName,
-                //onSaved: ,
-              ),
-              InputField(
-                name: "Last name",
-                keyboard: TextInputType.text,
-                fieldType: FieldType.lastName,
-              ),
-              InputField(
-                name: "Email address",
-                keyboard: TextInputType.emailAddress,
-                fieldType: FieldType.email,
-              ),
-              InputField(
-                name: "Phone number",
-                keyboard: TextInputType.phone,
-                fieldType: FieldType.phone,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  "User Role",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  //textAlign: TextAlign.left,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ProgressHUD(
+          child: Builder(
+            builder: (context) => Form(
+              key: _signupFormKey,
+              autovalidateMode: AutovalidateMode.disabled,
+              child: ListView(
                 children: [
-                  UserRole(
-                    name: "Farmer",
-                    valueProp: UserType.farmer,
-                  ),
-                  UserRole(
-                    name: "Distributor",
-                    valueProp: UserType.distributor,
-                  ),
-                ],
-              ),
-              InputField(
-                name: "Password",
-                iconVisible: true,
-                keyboard: TextInputType.visiblePassword,
-                fieldType: FieldType.password,
-              ),
-              // TODO: add spinner to button to indicate loading
-              GestureDetector(
-                onTap: () async {
-                  print("${_signupFormKey.currentState.validate()}");
-                  if (_signupFormKey.currentState.validate()) {
-                    try {
-                      final UserCredential _user =
-                          await auth.createUserWithEmailAndPassword(
-                        email:
-                            Provider.of<UserProv>(context, listen: false).email,
-                        password: Provider.of<UserProv>(context, listen: false)
-                            .password,
-                      );
-                      if (_user != null) {
-                        await store
-                            .collection("users")
-                            .doc(auth.currentUser.uid)
-                            .set(
-                          {
-                            "email": Provider.of<UserProv>(
-                              context,
-                              listen: false,
-                            ).email,
-                            "userId": _user.user.uid,
-                            "firstName": Provider.of<UserProv>(
-                              context,
-                              listen: false,
-                            ).firstName,
-                            "lastName": Provider.of<UserProv>(
-                              context,
-                              listen: false,
-                            ).lastname,
-                            "userRole": _roleStr[
-                                Provider.of<UserProv>(context, listen: false)
-                                    .role],
-                            "phoneNumber": Provider.of<UserProv>(
-                              context,
-                              listen: false,
-                            ).phoneNumber,
-                          },
-                        );
-                        print("Signup successful");
-                      }
-                    } catch (e) {
-                      Fluttertoast.showToast(
-                        msg: "${e.code}",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.BOTTOM,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.teal,
-                        textColor: Colors.white,
-                        fontSize: 16.0,
-                      );
-                    }
-                  }
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Card(
-                    color: Colors.green[400],
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Text(
-                        "Sign up",
-                        style: TextStyle(color: Colors.white),
-                        textAlign: TextAlign.center,
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, bottom: 20, top: 20),
+                    child: Text(
+                      "Create your account",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
                       ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account?",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
+                  InputField(
+                    name: "First name",
+                    keyboard: TextInputType.text,
+                    fieldType: FieldType.firstName,
+                    //onSaved: ,
+                  ),
+                  InputField(
+                    name: "Last name",
+                    keyboard: TextInputType.text,
+                    fieldType: FieldType.lastName,
+                  ),
+                  InputField(
+                    name: "Email address",
+                    keyboard: TextInputType.emailAddress,
+                    fieldType: FieldType.email,
+                  ),
+                  InputField(
+                    name: "Phone number",
+                    keyboard: TextInputType.phone,
+                    fieldType: FieldType.phone,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text(
+                      "User Role",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      //textAlign: TextAlign.left,
                     ),
                   ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      UserRole(
+                        name: "Farmer",
+                        valueProp: UserType.farmer,
+                      ),
+                      UserRole(
+                        name: "Distributor",
+                        valueProp: UserType.distributor,
+                      ),
+                    ],
+                  ),
+                  InputField(
+                    name: "Password",
+                    iconVisible: true,
+                    keyboard: TextInputType.visiblePassword,
+                    fieldType: FieldType.password,
+                  ),
                   GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Login(),
-                        ),
-                      );
-                      print("Navigate to Log in Screen");
+                    onTap: () async {
+                      // print("${_signupFormKey.currentState.validate()}");
+                      if (_signupFormKey.currentState.validate()) {
+                        final _progress = ProgressHUD.of(context);
+                        _progress.showWithText("Creating account...");
+                        await Future.delayed(
+                            Duration(seconds: 5)); // simulate progress
+                        try {
+                          final UserCredential _user =
+                              await auth.createUserWithEmailAndPassword(
+                            email: Provider.of<UserProv>(context, listen: false)
+                                .email,
+                            password:
+                                Provider.of<UserProv>(context, listen: false)
+                                    .password,
+                          );
+                          if (_user != null) {
+                            await store
+                                .collection("users")
+                                .doc(auth.currentUser.uid)
+                                .set(
+                              {
+                                "email": Provider.of<UserProv>(
+                                  context,
+                                  listen: false,
+                                ).email,
+                                "userId": _user.user.uid,
+                                "firstName": Provider.of<UserProv>(
+                                  context,
+                                  listen: false,
+                                ).firstName,
+                                "lastName": Provider.of<UserProv>(
+                                  context,
+                                  listen: false,
+                                ).lastname,
+                                "userRole": _roleStr[Provider.of<UserProv>(
+                                        context,
+                                        listen: false)
+                                    .role],
+                                "phoneNumber": Provider.of<UserProv>(
+                                  context,
+                                  listen: false,
+                                ).phoneNumber,
+                              },
+                            );
+                            _progress.dismiss();
+                            print("Signup successful");
+                          }
+                        } catch (e) {
+                          _progress.dismiss();
+                          Fluttertoast.showToast(
+                            msg: "${e.code}",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.teal,
+                            textColor: Colors.white,
+                            fontSize: 16.0,
+                          );
+                        }
+                      }
                     },
-                    child: Text(
-                      " Login",
-                      style: TextStyle(
-                          color: Colors.teal,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Card(
+                        color: Colors.green[400],
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            "Sign up",
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Login(),
+                            ),
+                          );
+                          print("Navigate to Log in Screen");
+                        },
+                        child: Text(
+                          " Login",
+                          style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
                 ],
               ),
-              SizedBox(
-                height: 10,
-              ),
-            ],
+            ),
           ),
         ),
       ),
