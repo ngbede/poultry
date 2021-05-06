9dcd6bb9f940193592b62d44408864f404528b2c
Emmanuel <ogasule001@gmail.com>
2021-05-06 21:53:49 +0100

    order ID generator function

---
+++ lib/config/id_gen.dart
@@ -0,0 +1,74 @@
+import 'dart:math' as math;
+
+// This is our custom ID generator used in creation of orders
+String generateID() {
+  final _random = math.Random();
+  final List<String> _alphabets = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  List<String> id = [];
+  for (int i = 0; i < 5; i++) {
+    _alphabets.shuffle();
+    if (i < 2) {
+      id.add(
+        _alphabets[_random.nextInt(52)],
+      );
+    } else {
+      id.add(
+        _random.nextInt(10).toString(),
+      );
+    }
+  }
+  return id.toString();
+}
