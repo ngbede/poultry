9e7eb8c70ec4a298b575a7f2e0972317c1136a8f
Emmanuel <ogasule001@gmail.com>
2021-05-13 23:17:05 +0100

    make drop down picker reusable

---
+++ lib/config/constants.dart
@@ -0,0 +1,59 @@
+final List<String> months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+final List<String> days = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+];
+
+final List<String> years = [
+  "2019",
+  "2020",
+  "2021",
+];
+
+final List<String> birdTypes = [
+  "Broiler",
+  "Layer",
+];
---
+++ lib/widgets/picker.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Container customPicker(
+  BuildContext context,
+  List items,
+  String chosenValue,
+  Function changeFunct,
+) {
+  List<DropdownMenuItem> itemList = [];
+  for (var item in items) {
+    var option = DropdownMenuItem(
+      child: Text(
+        " $item",
+      ),
+      value: item,
+    );
+    itemList.add(option);
+  }
+
+  return Container(
+    color: Color(0XFFEAE9EB),
+    child: DropdownButton(
+      items: itemList,
+      value: chosenValue,
+      onChanged: changeFunct,
+    ),
+  );
+}
