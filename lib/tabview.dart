6f1a137f8101cddfc92f8b348cdf63fbb03ab2bb
Emmanuel <ogasule001@gmail.com>
2021-05-15 09:10:20 +0100

    FIX: add check for null user uid before showing orders list

--- lib/tabview.dart
+++ lib/tabview.dart
@@ -10,54 +10,67 @@ class OrderTabView extends StatelessWidget {
   OrderTabView({@required this.orderStatus});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        StreamBuilder<DocumentSnapshot>(
-          stream: store
-              .collection("orders")
-              .doc("1234") // auth.currentUser.uid
-              .snapshots(),
-          builder: (context, snapshot) {
-            List<OrderCard> farmOrders = [];
-            if (snapshot.hasData) {
-              final orderList = snapshot.data.data().values;
-              for (var data in orderList) {
-                if (orderStatus == OrderStatus.open
-                    ? data["open"]
-                    : !data["open"]) {
-                  final bool open = data["open"];
-                  final String name = data["name"];
-                  final String address = data["address"];
-                  final String contact = data["contact"];
-                  final String id = data["ID"];
-                  final String status = open ? "PENDING" : "DELIVERED";
-                  final String date = data["date"];
-                  final int fertilizer = data["bagsOfFertilizer"];
-                  final int chickens = data["chickens"];
-                  final int eggCrates = data["cratesOfEggs"];
-                  final int productCount = data["products"];
-                  farmOrders.add(
-                    OrderCard(
-                      status: status,
-                      name: name,
-                      productCount: productCount,
-                      contact: contact,
-                      address: address,
-                      date: date,
-                      id: id,
+    return auth.currentUser != null
+        ? Column(
+            children: <Widget>[
+              StreamBuilder<DocumentSnapshot>(
+                stream: store
+                    .collection("orders")
+                    .doc(auth.currentUser.uid)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  List<OrderCard> farmOrders = [];
+                  if (snapshot.hasData) {
+                    final orderList = snapshot.data.data().values;
+                    for (var data in orderList) {
+                      if (orderStatus == OrderStatus.open
+                          ? data["open"]
+                          : !data["open"]) {
+                        final bool open = data["open"];
+                        final String name = data["name"];
+                        final String address = data["address"];
+                        final String contact = data["contact"];
+                        final String id = data["ID"];
+                        final String status = open ? "PENDING" : "DELIVERED";
+                        final String date = data["date"];
+                        // ignore: unused_local_variable
+                        final int fertilizer = data["bagsOfFertilizer"];
+                        // ignore: unused_local_variable
+                        final int chickens = data["chickens"];
+                        // ignore: unused_local_variable
+                        final int eggCrates = data["cratesOfEggs"];
+                        final int productCount = data["products"];
+                        farmOrders.add(
+                          OrderCard(
+                            status: status,
+                            name: name,
+                            productCount: productCount,
+                            contact: contact,
+                            address: address,
+                            date: date,
+                            id: id,
+                          ),
+                        );
+                      }
+                    }
+                  }
+                  return Expanded(
+                    child: ListView(
+                      children: farmOrders,
                     ),
                   );
-                }
-              }
-            }
-            return Expanded(
-              child: ListView(
-                children: farmOrders,
+                },
               ),
-            );
-          },
-        ),
-      ],
-    );
+            ],
+          )
+        : Center(
+            child: Text(
+              "No data to show at this time...",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          );
   }
 }
