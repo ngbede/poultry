37637b1d58711f49cfb776d36c5890ce76ed1e41
Emmanuel <ogasule001@gmail.com>
2021-06-02 12:55:11 +0100

    rebrand account UI

--- lib/screens/account_ui/account.dart
+++ lib/screens/account_ui/account.dart
@@ -1,12 +1,35 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:poultry/screens/account_ui/account_cards.dart';
 
 class Account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "Account Screen",
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AccountCard(
+          title: "Prices",
+          hintText: "Adjust product prices",
+          icon: FluentIcons.money_20_regular,
+        ),
+        AccountCard(
+          title: "Settings",
+          hintText: "Edit account deatils",
+          icon: FluentIcons.settings_20_regular,
+        ),
+        AccountCard(
+          title: "Customers",
+          hintText: "Store customer information",
+          icon: FluentIcons.person_add_20_regular,
+        ),
+        AccountCard(
+          title: "Sign Out",
+          hintText: "Log off from account",
+          icon: FluentIcons.sign_out_20_regular,
+        ),
+      ],
     );
   }
 }
---
+++ lib/screens/account_ui/account_cards.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AccountCard extends StatelessWidget {
+  final String title;
+  final String hintText;
+  final IconData icon;
+  AccountCard({
+    @required this.title,
+    @required this.hintText,
+    @required this.icon,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 20,
+            horizontal: 15,
+          ),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundColor: Color(0XFF35D4C0),
+              child: Icon(
+                icon,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0XFF35D4C0),
+                  ),
+                ),
+                Text(
+                  hintText,
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
