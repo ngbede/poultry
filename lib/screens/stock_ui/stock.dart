ea864497ef2e3a443aeef0ce75c68a420ca64b9d
Emmanuel <ogasule001@gmail.com>
2021-06-21 23:13:56 +0100

    add refresh indicator to stock screen

--- lib/screens/stock_ui/stock.dart
+++ lib/screens/stock_ui/stock.dart
@@ -13,64 +13,71 @@ class Stock extends StatefulWidget {
   _StockState createState() => _StockState();
 }
 
+// TODO: Fix async suspension error that occurs with transaction
 class _StockState extends State<Stock> {
   Future<void> updateStockUI() async {
-    //store.collection("stock_eggs").doc(auth.currentUser.uid).snapshots();
     DocumentReference documentRefEggs =
         store.collection('stock_eggs').doc(auth.currentUser.uid);
     DocumentReference documentRefChickens =
         store.collection('stock_chickens').doc(auth.currentUser.uid);
-    store.runTransaction(
-      (transaction) async {
-        DocumentSnapshot<Map> eggSnapshot =
-            await transaction.get(documentRefEggs);
-        DocumentSnapshot<Map> chickenSnapshot =
-            await transaction.get(documentRefChickens);
-        if (eggSnapshot.exists) {
-          if (eggSnapshot.data().containsKey(formatDate())) {
-            Provider.of<EggProv>(
-              context,
-              listen: false,
-            ).setPercent(1.0);
-          } else {
-            Provider.of<EggProv>(
-              context,
-              listen: false,
-            ).setPercent(0.0);
-          }
+    store.runTransaction((transaction) async {
+      DocumentSnapshot<Map> eggSnapshot =
+          await transaction.get(documentRefEggs);
+      DocumentSnapshot<Map> chickenSnapshot =
+          await transaction.get(documentRefChickens);
+      if (eggSnapshot.exists) {
+        if (eggSnapshot.data().containsKey(formatDate())) {
+          Provider.of<EggProv>(
+            context,
+            listen: false,
+          ).setPercent(1.0);
+        } else {
+          Provider.of<EggProv>(
+            context,
+            listen: false,
+          ).setPercent(0.0);
         }
-        if (chickenSnapshot.exists) {
-          Provider.of<BirdsProv>(context, listen: false).setNumberOfbatches(
-            chickenSnapshot.data().length,
-          );
-        }
-      },
-    );
+      }
+      if (chickenSnapshot.exists) {
+        Provider.of<BirdsProv>(context, listen: false).setNumberOfbatches(
+          chickenSnapshot.data().length,
+        );
+      }
+    });
   }
 
   @override
   // ignore: must_call_super
   void initState() {
+    super.initState();
     updateStockUI();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        StockCard(
-          name: "Chickens",
-          itemCount: Provider.of<BirdsProv>(context).batchCount,
-          reportCategory: StockReport.chickens,
+    return RefreshIndicator(
+      color: Color(0XFF35D4C0),
+      onRefresh: updateStockUI,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 5.0),
+        child: ListView(
+          children: [
+            StockCard(
+              name: "Chickens",
+              itemCount: Provider.of<BirdsProv>(context).batchCount,
+              reportCategory: StockReport.chickens,
+            ),
+            StockCard(
+              name: "Eggs",
+              itemCount: 1,
+              itemsCounted:
+                  Provider.of<EggProv>(context).percentage == 1.0 ? 1 : 0,
+              reportCategory: StockReport.eggs,
+              completionRate: Provider.of<EggProv>(context).percentage,
+            ),
+          ],
         ),
-        StockCard(
-          name: "Eggs",
-          itemCount: 1,
-          itemsCounted: Provider.of<EggProv>(context).percentage == 1.0 ? 1 : 0,
-          reportCategory: StockReport.eggs,
-          completionRate: Provider.of<EggProv>(context).percentage,
-        ),
-      ],
+      ),
     );
   }
 }
