d49e460bf52f0255c34e22bf5c1be547d6a1e2c9
Emmanuel <ogasule001@gmail.com>
2021-07-24 16:30:01 +0100

    reafactor toaster gravity property

--- lib/widgets/toast.dart
+++ lib/widgets/toast.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-void toaster(String message) async {
+void toaster(String message, ToastGravity position) async {
   Fluttertoast.showToast(
     msg: message,
     toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.BOTTOM,
+    gravity: position,
     timeInSecForIosWeb: 1,
     backgroundColor: Color(0XFF35D4C0),
     textColor: Colors.white,
